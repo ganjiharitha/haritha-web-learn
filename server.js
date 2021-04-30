209ab53bab814e1bca667549c669de7038b67270
ganjiharitha <ganjiharitha789@gmail.com>
2021-04-30 11:46:59 +0530

    resume added

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ app.get("/", function(req, res){
     res.send("Welcome to Ganji Haritha's Basic Site");
 })
 app.get("/resume", function(req, res){
-    file=__dirname+"/allhtmlfiles/resume.html";
+    file=__dirname+"/frontend/allhtmlfiles/resume.html";
     res.sendFile(file);
 })
 app.get("/game", function(req, res){
     res.send("Welcome to Haritha's Game Page");
 })
 // Heroku will automatically set an environment variable called PORT
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
  
 // Start the server
 app.listen(PORT, function(){
