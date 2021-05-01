4108f092a3b93f9439df0987552c67ec84a8666e
ganjiharitha <ganjiharitha789@gmail.com>
2021-05-01 14:45:20 +0530

    resumeicon

--- frontend/alljsfile/homepagejs.js
+++ frontend/alljsfile/homepagejs.js
@@ -1,5 +1,3 @@
 function resumefunction() {
-    homepage=__dirname+"/frontend/allhtmlfiles/homep.html";
-    res.sendFile(homepage);
-   //  window.location.replace("http://www.w3schools.com");
+    window.location.replace("https://haritha-web-learn.herokuapp.com/resume");
    }
