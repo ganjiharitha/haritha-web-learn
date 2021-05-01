78c75e8eea9693d32e346e09eda0986f86163c5a
ganjiharitha <ganjiharitha789@gmail.com>
2021-05-01 14:38:30 +0530

    resumeicon

--- frontend/alljsfile/homepagejs.js
+++ frontend/alljsfile/homepagejs.js
@@ -1,1 +1,5 @@
-console.log("javascript homepage loaded");
+function resumefunction() {
+    homepage=__dirname+"/frontend/allhtmlfiles/homep.html";
+    res.sendFile(homepage);
+   //  window.location.replace("http://www.w3schools.com");
+   }
