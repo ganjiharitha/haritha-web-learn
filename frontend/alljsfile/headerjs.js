dcd36b6c20f4adac34e9dab6f4d0fdfd37cb4c6e
ganjiharitha <ganjiharitha789@gmail.com>
2021-05-03 17:04:04 +0530

    homepage added

--- frontend/alljsfile/headerjs.js
+++ frontend/alljsfile/headerjs.js
@@ -1,3 +1,3 @@
 function restohomefun() {
-     window.location.replace("https://haritha-web-learn.herokuapp.com/");
+     window.location.replace("https://haritha-web-learn.herokuapp.com/homepage");
      }
