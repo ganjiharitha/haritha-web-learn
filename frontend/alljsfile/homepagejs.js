9c5ec1891e6df22f63337ce1a2b6f892510454d1
ganjiharitha <ganjiharitha789@gmail.com>
2021-05-04 09:20:04 +0530

    back is added

--- frontend/alljsfile/homepagejs.js
+++ frontend/alljsfile/homepagejs.js
@@ -2,3 +2,6 @@ function resumefunction() {
     console.log("resumeloading");
     window.location.replace("https://haritha-web-learn.herokuapp.com/resume");
    }
+   function restohomefun() {
+     window.location.replace("https://haritha-web-learn.herokuapp.com/");
+     }
