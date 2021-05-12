626905e2f454f5a7171654607cea7074e3a1c3bf
ganjiharitha <ganjiharitha789@gmail.com>
2021-05-12 14:23:58 +0530

    register added

--- frontend/alljsfile/loginjs.js
+++
@@ -1,9 +0,0 @@
-function submitfun()
-{
-    u = document.getElementById("uname").value;
-    p = document.getElementById("upass").value;
-    if(u=="admin" && p=="admin")
-        window.location.href ="https://haritha-web-learn.herokuapp.com/";
-    else
-    alert("enter valid credentials");
-}
